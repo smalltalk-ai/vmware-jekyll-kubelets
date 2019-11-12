388310fd1fad46cfeb588be40729958787554404
Colin Bayer <ccbayer@gmail.com>
2019-11-12 11:39:42 -0800

    design updates

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,8 @@
 
   $('ul#header-nav li.' + $('body').attr('id')).addClass('selected');
   
-  // modal
+  /* MODAL */
+  
   $('#playerModal').on('shown.bs.modal', function (e) {
     var 
       $this = $('#playerModal'),
@@ -33,6 +34,23 @@
     $iframe.attr('src', '');
     // clear title
     $this.find('h5').html('Loading...');
+  });
+  
+  /* LOAD MORE */
+  // via https://tobiasahlin.com/blog/infinite-scroll-with-jekyll/
+  // https://github.com/tobiasahlin/infinite-jekyll
+  // Load the JSON file containing all URLs
+  var 
+    allEpisodes,
+    $target = $('#ajax-target')
+  ;
+  
+  $.getJSON("/episodes.json", function(data) {
+    allEpisodes = data["episodes"];
+  });
+    
+  $('a.load-more').click(function() {
+    $target.append(allEpisodes[1]);
   });
   
 })();
